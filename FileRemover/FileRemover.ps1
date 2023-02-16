f6b259fa8ceeae41aca53764d5e05ca05ff9827a
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2023-02-16 15:22:38 +0000

    Add files via upload

---
+++ FileRemover/FileRemover.ps1
@@ -0,0 +1,8 @@
+﻿$RemoveData = ./ConfirmedRemove.csv
+
+$ImportCSV = import-csv -Path $RemoveData
+foreach($Line in $ImportCSV){
+    $FilePath = $Line.FullName
+    Write-host $FilePath
+    Remove-Item -Path $FilePath -Force
+}
---
+++ FileReporter/OldFIleReport.ps1
@@ -0,0 +1,14 @@
+﻿# Jared Stillwell
+
+$rootdir = "E:\Data\"
+$YearsBack = "-10"
+
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddYears($YearsBack)
+cd $rootdir
+$listfolder = ls
+foreach($folder in $listfolder){
+    $FileName = $folder.Name
+    $ReportName = "C:\Temp\scripts\Report\Test\$FileName.csv"
+    Get-ChildItem $FileName -Recurse | Where-Object {$_.LastWriteTime -lt $DatetoDelete -AND $_.PSIsContainer -like 'FALSE'} | Select-Object FullName,CreationTime,LastAccessTime,LastWriteTime | Export-csv -Path $ReportName -NoTypeInformation
+}
